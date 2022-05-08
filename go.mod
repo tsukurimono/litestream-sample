7bcbe757ce64d34cd414513f5719d1e7fccb0134
tsukurimono1 <tsukurimono1>
2022-05-09 00:48:50 +0900

    mod

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
-module github.com/tsukurimono/litestream-sample-app
+module litestream-sample-app
 
 go 1.15
 
-require github.com/mattn/go-sqlite3 v1.14.12 // indirect
+require github.com/mattn/go-sqlite3 v1.14.12
--- main.go
+++ main.go
@@ -1,42 +1,46 @@
 package main
 
 import (
-    "database/sql"
     "fmt"
     "time"
-    _ "github.com/mattn/go-sqlite3"
+
+    "litestream-sample-app/database/sqlite3"
 )
 
 func main() {
-    db, err := sql.Open("sqlite3", "./foo.db")
-    checkErr(err)
+    db, err := sqlite3.New()
+    if err != nil {
+		panic(err)
+	}
 
-    //データの挿入
-    stmt, err := db.Prepare("INSERT INTO userinfo(username, departname, created) values(?,?,?)")
-    checkErr(err)
+	result, err := db.Exec("INSERT INTO userinfo(username, departname, created) values(?,?,?)", "astaxie", "研究開発部門", "2012-12-09")
+    if err != nil {
+		panic(err)
+	}
 
-    res, err := stmt.Exec("astaxie", "研究開発部門", "2012-12-09")
-    checkErr(err)
-
-    id, err := res.LastInsertId()
-    checkErr(err)
+    id, err := result.LastInsertId()
+    if err != nil {
+		panic(err)
+	}
 
     fmt.Println(id)
-    //データの更新
-    stmt, err = db.Prepare("update userinfo set username=? where uid=?")
-    checkErr(err)
 
-    res, err = stmt.Exec("astaxieupdate", id)
-    checkErr(err)
+    result, err = db.Exec("update userinfo set username=? where uid=?", "astaxieupdate", id)
+    if err != nil {
+		panic(err)
+	}
 
-    affect, err := res.RowsAffected()
-    checkErr(err)
+    affect, err := result.RowsAffected()
+    if err != nil {
+		panic(err)
+	}
 
     fmt.Println(affect)
 
-    //データの検索
     rows, err := db.Query("SELECT * FROM userinfo")
-    checkErr(err)
+    if err != nil {
+		panic(err)
+	}
 
     for rows.Next() {
         var uid int
@@ -44,31 +48,27 @@ func main() {
         var department string
         var created time.Time
         err = rows.Scan(&uid, &username, &department, &created)
-        checkErr(err)
+        if err != nil {
+		    panic(err)
+	    }
+
         fmt.Println(uid)
         fmt.Println(username)
         fmt.Println(department)
         fmt.Println(created)
     }
 
-    //データの削除
-    stmt, err = db.Prepare("delete from userinfo where uid=?")
-    checkErr(err)
+    result, err = db.Exec("delete from userinfo where uid=?", id)
+    if err != nil {
+		panic(err)
+	}
 
-    res, err = stmt.Exec(id)
-    checkErr(err)
-
-    affect, err = res.RowsAffected()
-    checkErr(err)
+    affect, err = result.RowsAffected()
+    if err != nil {
+		panic(err)
+	}
 
     fmt.Println(affect)
 
-    db.Close()
-
-}
-
-func checkErr(err error) {
-    if err != nil {
-        panic(err)
-    }
+    //db.Close()
 }
