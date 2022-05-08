1098618202fef347abecf638ad16e7ff16294151
tsukurimono1 <tsukurimono1>
2022-05-08 16:57:31 +0900

    sqlite sample

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,15 @@ FROM golang:1.15.7-alpine as dev
 ENV ROOT=/go/src/litestream-sample
 ENV GOMODCACHE=/go/cache/go_mod
 ENV GO111MODULE=on
-ENV CGO_ENABLED 0
+ENV CGO_ENABLED 1
 WORKDIR ${ROOT}
 
 RUN apk update && apk add git \
     vim \
-    bash
+    bash \
+    gcc \
+    libc-dev \
+    sqlite \
+    sqlite-dev
 
 CMD ["bash"]
--- main.go
+++ main.go
@@ -1,5 +1,74 @@
 package main
-import "fmt"
+
+import (
+    "database/sql"
+    "fmt"
+    "time"
+    _ "github.com/mattn/go-sqlite3"
+)
+
 func main() {
-    fmt.Println("hello world")
+    db, err := sql.Open("sqlite3", "./foo.db")
+    checkErr(err)
+
+    //データの挿入
+    stmt, err := db.Prepare("INSERT INTO userinfo(username, departname, created) values(?,?,?)")
+    checkErr(err)
+
+    res, err := stmt.Exec("astaxie", "研究開発部門", "2012-12-09")
+    checkErr(err)
+
+    id, err := res.LastInsertId()
+    checkErr(err)
+
+    fmt.Println(id)
+    //データの更新
+    stmt, err = db.Prepare("update userinfo set username=? where uid=?")
+    checkErr(err)
+
+    res, err = stmt.Exec("astaxieupdate", id)
+    checkErr(err)
+
+    affect, err := res.RowsAffected()
+    checkErr(err)
+
+    fmt.Println(affect)
+
+    //データの検索
+    rows, err := db.Query("SELECT * FROM userinfo")
+    checkErr(err)
+
+    for rows.Next() {
+        var uid int
+        var username string
+        var department string
+        var created time.Time
+        err = rows.Scan(&uid, &username, &department, &created)
+        checkErr(err)
+        fmt.Println(uid)
+        fmt.Println(username)
+        fmt.Println(department)
+        fmt.Println(created)
+    }
+
+    //データの削除
+    stmt, err = db.Prepare("delete from userinfo where uid=?")
+    checkErr(err)
+
+    res, err = stmt.Exec(id)
+    checkErr(err)
+
+    affect, err = res.RowsAffected()
+    checkErr(err)
+
+    fmt.Println(affect)
+
+    db.Close()
+
+}
+
+func checkErr(err error) {
+    if err != nil {
+        panic(err)
+    }
 }
